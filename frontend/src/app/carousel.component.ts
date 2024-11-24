8122e9449c6e62749049c90950784519d6dcee35
Riyaz J <j.riyazu@gmail.com>
2024-11-24 17:46:10 +0530

    TODO: Tailwindcss working, organize it properly

--- frontend/src/app/carousel.component.ts
+++ frontend/src/app/carousel.component.ts
@@ -6,30 +6,40 @@ import { CommonModule } from "@angular/common";
   selector: "app-carousel",
   imports: [CommonModule],
   template: `
-    <table class="table m-3">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Name</th>
-          <th scope="col">User-Name</th>
-          <th scope="col">Email</th>
-          <th scope="col">Phone</th>
-        </tr>
-      </thead>
-      <tbody>
-        @for (user of userService.users(); track $index) {
-          <tr>
-            <th scope="row">{{ user.id }}</th>
-            <td>{{ user.name }}</td>
-            <td>{{ user.username }}</td>
-            <td>{{ user.email }}</td>
-            <td>{{ user.phone }}</td>
-          </tr>
-        } @empty {
-          <td colspan="4">No users found ...</td>
-        }
-      </tbody>
-    </table>
+    <div class="flex flex-col">
+      <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+          <div class="overflow-hidden">
+            <table
+                class="min-w-full text-left text-sm font-light text-surface dark:text-white">
+              <thead
+                  class="border-b border-neutral-200 font-medium dark:border-white/10">
+              <tr>
+                <th scope="col" class="px-6 py-4">#</th>
+                <th scope="col" class="px-6 py-4">Name</th>
+                <th scope="col" class="px-6 py-4">User Name</th>
+                <th scope="col" class="px-6 py-4">Email</th>
+                <th scope="col" class="px-6 py-4">Phone</th>
+              </tr>
+              </thead>
+              <tbody>
+                @for (user of userService.users(); track $index) {
+                  <tr class="border-b border-neutral-200 dark:border-white/10">
+                    <th class="whitespace-nowrap px-6 py-4 font-medium">{{ user.id }}</th>
+                    <td class="whitespace-nowrap px-6 py-4">{{ user.name }}</td>
+                    <td>{{ user.username }}</td>
+                    <td>{{ user.email }}</td>
+                    <td>{{ user.phone }}</td>
+                  </tr>
+                } @empty {
+                  <td colspan="4">No users found ...</td>
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   `,
   styles: ``,
 })
--- frontend/src/app/not-found.component.ts
+++ frontend/src/app/not-found.component.ts
@@ -5,24 +5,20 @@ import { RouterLink } from "@angular/router";
   selector: "app-not-found",
   imports: [RouterLink],
   template: `
-    <div class="container m-3">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="error-template">
-            <h1>Oops!</h1>
-            <h2>404 Not Found</h2>
-            <div class="error-details">
-              Sorry, an error has occurred. Requested page not found!
-            </div>
-            <div class="error-actions">
-              <a routerLink="/example" class="btn btn-primary btn-lg">
-                <span class="glyphicon glyphicon-home"></span> Take Me Home
-              </a>
-            </div>
-          </div>
+    <section class="flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800">
+      <div class="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <div class="max-w-md text-center">
+          <h2 class="mb-8 font-extrabold text-9xl dark:text-gray-400">
+            <span class="sr-only">Error</span>404
+          </h2>
+          <p class="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
+          <p class="mt-4 mb-8 dark:text-gray-600">But dont worry, you can find plenty of other things on our
+            homepage.</p>
+          <a rel="noopener noreferrer" routerLink="/example"
+             class="px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Back to homepage</a>
         </div>
       </div>
-    </div>
+    </section>
   `,
   styles: ``,
 })
