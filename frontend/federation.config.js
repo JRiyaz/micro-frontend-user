bafe9f16cb904afcd6d5dcfa731df9f58e4c90a5
Riyaz J <j.riyazu@gmail.com>
2025-08-02 16:57:44 +0530

    Change the dir name from user to user-service

--- frontend/federation.config.js
+++ frontend/federation.config.js
@@ -1,30 +1,30 @@
 const {
   withNativeFederation,
   shareAll,
-} = require("@angular-architects/native-federation/config");
+} = require('@angular-architects/native-federation/config');
 
 module.exports = withNativeFederation({
-  name: "user-app",
+  name: 'user-app',
 
   exposes: {
-    "./Sample-Component":
-      "./projects/user/frontend/src/app/sample.component.ts",
-    "./USER-ROUTES": "./projects/user/frontend/src/app/app.routes.ts",
+    './Sample-Component':
+      './projects/user-service/frontend/src/app/sample.component.ts',
+    './USER-ROUTES': './projects/user-service/frontend/src/app/app.routes.ts',
   },
 
   shared: {
     ...shareAll({
       singleton: true,
       strictVersion: true,
-      requiredVersion: "auto",
+      requiredVersion: 'auto',
     }),
   },
 
   skip: [
-    "rxjs/ajax",
-    "rxjs/fetch",
-    "rxjs/testing",
-    "rxjs/webSocket",
+    'rxjs/ajax',
+    'rxjs/fetch',
+    'rxjs/testing',
+    'rxjs/webSocket',
     // Add further packages you don't need at runtime
   ],
 
