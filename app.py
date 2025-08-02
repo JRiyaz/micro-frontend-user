3b9217a7f16788a0fee32bd16979cb02027647cb
Riyaz J <j.riyazu@gmail.com>
2025-08-02 18:05:17 +0530

    Rename asgi.py to app.py as per Fastapi's naming convention

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+
 from user.main import create_app
 
 app: FastAPI = create_app()
 # uv run uvicorn --factory user.main:create_app
-# uv run uvicorn asgi:app
-# uv run fastapi dev asgi.py
+# uv run uvicorn app:app
+# uv run fastapi dev app.py
