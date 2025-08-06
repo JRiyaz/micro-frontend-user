4fd383253c1947b46073fa12ea385f0352ed7da9
Riyaz J <j.riyazu@gmail.com>
2025-08-07 01:06:22 +0530

    new sqlalchemy syntax

--- user/sample_db.py
+++ user/sample_db.py
@@ -1,42 +1,64 @@
-from uuid import uuid4
+from typing import Optional, Sequence
+from uuid import UUID
 
-from sqlalchemy import UUID, Boolean, Column, Integer, String, create_engine
-from sqlalchemy.orm import DeclarativeBase, Mapped, Session
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import DeclarativeBase, Mapped, Session, load_only, mapped_column
 
 CONNECTION_STRING = "postgresql+psycopg2://admin:admin@localhost/fastapi"
 
 engine = create_engine(CONNECTION_STRING, echo=False)
 
+"""
+session.query(User).filter_by(active=True).all() ==> [generated in 0.00021s]
+SELECT users.id AS users_id, users.username AS users_username, users.email AS users_email, users."firstName" AS "users_firstName", users."lastName" AS "users_lastName", users.password AS users_password, users.age AS users_age, users.active AS users_active 
+FROM users 
+WHERE users.active = true
+
+session.scalars(select(User).where(User.active.is_(True))).all() ==> [generated in 0.00017s]
+SELECT users.id, users.username, users.email, users."firstName", users."lastName", users.password, users.age, users.active 
+FROM users 
+WHERE users.active IS true
+"""
+
 
 class Base(DeclarativeBase):
-    __abstract__ = True
+    pass
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[UUID] = Column(UUID, primary_key=True, default=uuid4)
-    username: Mapped[str] = Column(String(64), unique=True, nullable=False)
-    email: Mapped[str] = Column(String(64), unique=True, nullable=False)
-    firstName: Mapped[str] = Column(String(64), nullable=False)
-    lastName: Mapped[str] = Column(String(64), nullable=False)
-    password: Mapped[str] = Column(String(64))
-    active: Mapped[bool] = Column(Boolean)
-    age: Mapped[int] = Column(Integer)
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    firstName: Mapped[str]
+    lastName: Mapped[str]
+    password: Mapped[Optional[str]]
+    age: Mapped[int]
+    active: Mapped[bool] = mapped_column(default=False)
 
     def __repr__(self):
         return (
-            f"<User(id={self.id}, username={self.username}, email={self.email}, firstName={self.firstName}, "
-            f"lastName={self.lastName}, active={self.active}, age={self.age})>"
+            f"<User(id={self.id}, username={self.username}, email={self.email}, active={self.active}, "
+            f"firstName={self.firstName}, lastName={self.lastName})>"
         )
 
 
 Base.metadata.create_all(engine)
 
-with Session(bind=engine) as session:
-    # session.add_all(users)
-    # session.commit()
+with Session(engine) as session:
+    # stmt = select(User).where(User.active.is_(True))
+    # users = session.scalars(stmt).all()
+    # for user in users:
+    #     print(user)
 
-    users = session.query(User).all()
-    for user in users:
+    stmt = select(User).where(User.firstName.in_(["Azu", "Khan"]))
+    result: Sequence[User] = session.scalars(stmt).all()
+    for user in result:
+        user.active = False
+    session.commit()
+
+    res = session.scalars(select(User).where(User.active.is_(False))).all()
+
+    for user in res:
         print(user)
