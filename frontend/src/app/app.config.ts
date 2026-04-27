de41ef32d50445dbb9934262e977a778031b9813
Riyaz J <j.riyazu@gmail.com>
2026-04-28 00:32:13 +0530

    Upgrade Angular to 21.2.x

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import {
   ApplicationConfig,
-  provideExperimentalZonelessChangeDetection,
+  provideZonelessChangeDetection,
 } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
@@ -9,7 +9,7 @@ import { provideHttpClient } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(),
   ],
